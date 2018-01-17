0fc2ec0684a8f6ca437ac2dccd4933bd3fec6b41
Mihail Oprea <mishanea@Mihails-MacBook-Pro.local>
2018-01-17 10:40:01 -0800

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title:string;
+  colors:string[];
+  barcode:string[];
+
+  constructor(){
+    this.title='Welcome To The Retro Barcode!';
+    this.colors=['#DC143C','#00FFFF','#B8860B','#FF00FF','#008000','#FF4500'];
+    this.barcode=[];
+    for(let i=0;i<10;i++){
+      this.barcode.push(this.colors[Math.floor(Math.random()*this.colors.length)]);
+    }
+  }
 }
